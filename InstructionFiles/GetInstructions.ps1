bdd111b9233a36be470ea1893129fcee5a608916
Arjun Dube <adube@vmware.com>
2020-05-04 10:01:49 -0700

    Updates GetInstructions to honor number of pages

--- InstructionFiles/GetInstructions.ps1
+++ InstructionFiles/GetInstructions.ps1
@@ -7,7 +7,7 @@ Workflow Get-Instructions {
     $UrlFormat = "https://lego.brickinstructions.com/{0}/{1}/{2:D3}.jpg"
     $SetGroupId = $SetId - ($SetId  % 1000);
     New-Item -ItemType Directory -Force -Path "./$SetId"
-    foreach -parallel -throttlelimit 100 ( $i in 1..72 ) {
+    foreach -parallel -throttlelimit 100 ( $i in 1..$NumPages ) {
         Invoke-WebRequest -Uri ($UrlFormat -f $SetGroupId,$SetId,$i) -OutFile "$SetId/$i.jpg"
     }
     #for ($i=1; $i -le $NumPages; $i++) {   
